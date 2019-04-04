5a418ecad969789ade5d724f9c11f4d2cd1c4879
Michael Valentiner <michael.valentiner@gmail.com>
2019-04-04 09:28:03 -0400

    Working progress. Test are broken.

---
+++ PlacesUnitTests/NetworkDataJSONTests.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkDataJSONTests.swift
+//  PlacesUnitTests
+//
+//  Created by Michael Valentiner on 3/25/19.
+//  Copyright © 2019 Michael Valentiner. All rights reserved.
+//
+
+import XCTest
+@testable import Places
+
+class NetworkDataJSONTests: XCTestCase {
+
+    func testFlickrRequest() {
+		class SuccessFlickrDataRequest : UnauthenticatedDataRequest {
+			typealias RequestedDataType = [String : JSON]
+			var endpointURL: String {
+				get { return "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=3a95d51756054b5b3e1cf23ff6b9f945&format=json&extras=geo,media=photos,url_s,url_t,url_q,url_k,url_h,url_b,url_c,url_z&sort=interestingness-desc&nojsoncallback=1&per_page=4&page=0&" }
+			}
+		}
+
+    	let expectation = XCTestExpectation(description: "Test NetworkDataRequestTests.testDataRequest_Load")
+		let dataRequest = SuccessFlickrDataRequest()
+		do {
+			_ = try dataRequest.load().done { photoInfo in
+//				print("\(photoInfo)")
+				XCTAssertTrue(photoInfo["stat"] == "ok")
+				expectation.fulfill()
+			}.catch { error in
+				print("error = \(error)")
+				XCTAssertTrue(false)
+				expectation.fulfill()
+			}
+		} catch (let error) {
+			XCTAssertTrue(false, "error = \(error)")
+			expectation.fulfill()
+		}
+
+		wait(for: [expectation], timeout: 10.0)
+    }
+}
---
+++ PlacesUnitTests/NetworkDataRequestTests.swift
@@ -0,0 +1,122 @@
+//
+//  NetworkDataRequestTests.swift
+//  Places NearTests
+//
+//  Created by Michael Valentiner on 3/15/19.
+//  Copyright © 2019 Michael Valentiner. All rights reserved.
+//
+
+import XCTest
+@testable import Places
+
+class NetworkDataRequestTests: XCTestCase {
+	private enum CodingKeys : String, CodingKey {
+		case results
+		case version
+	}
+	struct GoodAppStoreInfo : Decodable {
+		internal let version : String
+		init(from decoder: Decoder) throws {
+			// {"results": [{"version":"2.2"},]}
+			let container = try decoder.container(keyedBy: CodingKeys.self)
+			var resultArray	= try container.nestedUnkeyedContainer(forKey: .results)
+			let result = try resultArray.nestedContainer(keyedBy: CodingKeys.self)
+			let version = try result.decode(String.self, forKey: .version)
+			self.version = version
+		}
+	}
+	func testDataRequest_Load_Success() {
+		class SuccessTestDataRequest : UnauthenticatedDataRequest {
+			typealias RequestedDataType = GoodAppStoreInfo
+			var endpointURL: String {
+				get { return "https://itunes.apple.com/lookup?bundleId=com.heliotropix.Photos-Near" }
+			}
+		}
+
+    	let expectation = XCTestExpectation(description: "Test NetworkDataRequestTests.testDataRequest_Load")
+		let dataRequest = SuccessTestDataRequest()
+		do {
+			_ = try dataRequest.load().done { appStoreInfo in
+				let versionNumber = Double(appStoreInfo.version)
+				XCTAssertTrue(versionNumber != nil)
+				expectation.fulfill()
+			}.catch { error in
+				print("error = \(error)")
+				XCTAssertTrue(false)
+				expectation.fulfill()
+			}
+		} catch (let error) {
+			XCTAssertTrue(false, "error = \(error)")
+			expectation.fulfill()
+		}
+
+		wait(for: [expectation], timeout: 10.0)
+	}
+
+	//** testDataRequest_BadDecode_Fail
+	func testDataRequest_BadDecode_Fail() {
+		struct BadAppStoreInfo : Decodable {
+			internal let version : String
+			init(from decoder: Decoder) throws {
+				// {"results": [{"version":"2.2"},]}
+				let container = try decoder.container(keyedBy: CodingKeys.self)
+				let version = try container.decode(String.self, forKey: .version)
+				self.version = version
+			}
+		}
+		class BadDecodeTestDataRequest : UnauthenticatedDataRequest {
+			typealias RequestedDataType = BadAppStoreInfo
+			var endpointURL: String {
+				get { return "https://itunes.apple.com/lookup?bundleId=com.heliotropix.Photos-Near" }
+			}
+		}
+
+    	let expectation = XCTestExpectation(description: "Test NetworkDataRequestTests.testDataRequest_Load")
+		let dataRequest = BadDecodeTestDataRequest()
+		do {
+			_ = try dataRequest.load().done { _ in
+				XCTAssertTrue(false)
+				expectation.fulfill()
+			}.catch { error in
+				print("error = \(error)")
+				XCTAssertTrue(true)
+				expectation.fulfill()
+			}
+		} catch (let error) {
+			print("error = \(error)")
+			XCTAssertTrue(true)
+			expectation.fulfill()
+		}
+
+		wait(for: [expectation], timeout: 10.0)
+	}
+
+	//** testDataRequest_BadEndpoint_Fail
+	func testDataRequest_BadEndpoint_Fail() {
+		class BadEndpointTestDataRequest : UnauthenticatedDataRequest {
+			typealias RequestedDataType = GoodAppStoreInfo
+			var endpointURL: String {
+				get { return "https://foo.bar" }
+			}
+		}
+
+    	let expectation = XCTestExpectation(description: "Test NetworkDataRequestTests.testDataRequest_Load")
+		let dataRequest = BadEndpointTestDataRequest()
+		do {
+			_ = try dataRequest.load().done { _ in
+				XCTAssertTrue(false)
+				expectation.fulfill()
+			}.catch { error in
+				print("error = \(error)")
+				XCTAssertTrue(true)
+				expectation.fulfill()
+			}
+		} catch (let error) {
+			print("error = \(error)")
+			XCTAssertTrue(true)
+			expectation.fulfill()
+		}
+
+		wait(for: [expectation], timeout: 10.0)
+	}
+}
